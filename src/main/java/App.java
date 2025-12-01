0613252884b41db3b29c31fd526a9675cb1512b1
Vasiliy <exs777@mail.ru>
2025-12-01 15:21:18 +0700

    Initial commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,13 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld beanHw =
+                (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        System.out.println(beanHw==bean);
+        Cat beanCat=(Cat) applicationContext.getBean("Cat");
+        Cat beanCatNew=(Cat) applicationContext.getBean("Cat");
+        System.out.println(beanCat.getName());
+        System.out.println(beanCatNew==beanCat);
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,13 @@
+import java.util.PrimitiveIterator;
+
+public class Cat {
+    private  String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
